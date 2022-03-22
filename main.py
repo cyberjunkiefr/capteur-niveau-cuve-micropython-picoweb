befb2c1de75d6fb202d9a428d7ea3dfb515d203f
François Parola <fpachat@gmail.com>
2022-03-22 21:15:23 +0100

    main modifié

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from machine import *
 from hcsr04 import HCSR04
 import time, utime, wificonnect, tft_config, st7789, picoweb
-import vga1_bold_16x32, vga2_16x16, vga1_8x8  # font
+import vga1_bold_16x32, vga2_16x16 # font
+
 
 
 # ------------------------------------------ TANK SIZE -------------------------------------------------------------------------
@@ -17,41 +18,57 @@ dimension_cuve_Y = 5.68;  # Y size of the tank / taille de la cuve en Y en m
 
 
 # definition pin module
-led_bleu = Pin(27, Pin.OUT, 0)
+led_bleu = Pin(27, Pin.OUT, 1)
 led_verte1 = Pin(26, Pin.OUT, 0)
-led_verte2 = Pin(25, Pin.OUT, 0)
+led_verte2 = Pin(25, Pin.OUT, 1)
 led_jaune1 = Pin(33, Pin.OUT, 0)
-led_jaune2 = Pin(32, Pin.OUT, 0)
+led_jaune2 = Pin(32, Pin.OUT, 1)
 led_rouge = Pin(12, Pin.OUT, 0)
 buzzer = Pin(15, Pin.OUT, 0)
 
 # definition du display
 tft = tft_config.config()
-
-# definition variables:
-surface_cuve = dimension_cuve_X * dimension_cuve_Y;
-volume_max_cuve = round(surface_cuve * hauteur_max_eau, 2)
-volume_disponible = 0.00
+tft.init()
+tft.sleep_mode(1) # tft off
 
 # connexion au wifi
 ipaddress = wificonnect.connectSTA(ssid='PAROLA_WIFI', password='LilieLuluKelia25')
 print(ipaddress)
 
+# definition variables:
+surface_cuve = dimension_cuve_X * dimension_cuve_Y;
+volume_max_cuve = round(surface_cuve * hauteur_max_eau, 2)
+volume_disponible = 0.00
+timer = Timer(0)
+
+
+def led_init():
+    led_bleu.value(0)
+    led_verte1.value(0)
+    led_verte2.value(0)
+    led_jaune1.value(0)
+    led_jaune2.value(0)
+    led_rouge.value(0)
+
 
 def calcul_volume():
+    global volume_disponible
     sensor = HCSR04(trigger_pin=22, echo_pin=21)
     try:
         distance = sensor.distance_cm() / 100 # distance en m
         volume_disponible=round(((hauteur_max_eau + position_capteur) * surface_cuve) - (distance * surface_cuve), 2)
-        print("Distance: ", distance, " m\nVolume d'eau: ", volume_disponible, " m3")
+        print("Distance: ", distance, " m\nVolume disponible: ", volume_disponible, " m3")
         return volume_disponible
-    except OSError as error:
-        print('Sensor error: ', error)
+    except:
+        print('error calcul_volume')
         error()
-
+        
+        
        
 def affichage():
-    tft.init()
+    led_init()
+    tft.sleep_mode(0)
+    global volume_disponible
     volume_disponible = calcul_volume()
     if volume_max_cuve > volume_disponible > 0:
         if volume_disponible >= 0.9*volume_max_cuve:
@@ -68,40 +85,36 @@ def affichage():
             led_rouge.value(1)
         else:
             buzzer.value(1)
-            while True:
-                led_rouge.value(not led_rouge.value())
-                time.sleep(1)
-
-        # affichage digital
-        tft.text(vga1_bold_16x32, "NIVEAU CUVE", 35, 15, st7789.CYAN)
-        tft.text(vga2_16x16, "Capacite: 10m3", 6, 60, st7789.YELLOW)
-        tft.text(vga2_16x16, "Reste: ", 15, 95, st7789.GREEN)
-        tft.text(vga2_16x16, str(volume_disponible), 120, 95, st7789.GREEN)
-        tft.text(vga2_16x16, "m3", 190, 95, st7789.GREEN)
-        return volume_disponible
-
-    else: 
-        while True:
             led_rouge.value(not led_rouge.value())
-            time.sleep(0.1)
+            tft.fill(st7789.YELLOW)
+            tft.text(vga1_bold_16x32, "Cuve quasi", 40, tft.height() // 3 - vga1_bold_16x32.HEIGHT//2, st7789.RED, st7789.YELLOW)
+            tft.text(vga1_bold_16x32, "vide", 70, (tft.height() // 3)*2 - vga1_bold_16x32.HEIGHT//2, st7789.RED, st7789.YELLOW)
+            time.sleep(1)
+        affichage_digital()
+
+
+def affichage_digital():
+    global volume_disponible
+    tft.text(vga1_bold_16x32, "NIVEAU CUVE", 35, 15, st7789.CYAN)
+    tft.text(vga2_16x16, "Capacite: 10m3", 6, 60, st7789.YELLOW)
+    tft.text(vga2_16x16, "Reste: ", 15, 95, st7789.GREEN)
+    tft.text(vga2_16x16, str(volume_disponible), 120, 95, st7789.GREEN)
+    tft.text(vga2_16x16, "m3", 190, 95, st7789.GREEN)
 
 
 def error():
     tft.init()
+    tft.sleep_mode(0)
     tft.text(vga1_bold_16x32, "MESURE", 80, tft.height() // 3 - vga1_bold_16x32.HEIGHT//2, st7789.RED, st7789.BLACK)
     text = "ERRONEE"
     length=len(text)
     tft.text(vga1_bold_16x32, text, tft.width() // 2 - length // 2 * vga1_bold_16x32.WIDTH, 2 * tft.height() // 3 - vga1_bold_16x32.HEIGHT//2, st7789.RED, st7789.BLACK)
-    text = "Reset manual nécessaire"
-    length=len(text)
-    tft.text(vga1_8x8, text, tft.width() // 2 - length // 2 * vga1_8x8.WIDTH, 2 * tft.height() // 2 - vga1_8x8.HEIGHT//2, st7789.RED, st7789.BLACK)
-    utime.sleep(1)
 
-
-timer = Timer(0)
+ 
 def handleInterrupt(timer):
-    global volume_disponible
     affichage()
+    #timer.init(period=300000, mode=Timer.ONE_SHOT, callback=handleInterrupt)
+
  
 # ---- Routing Picoweb ------------------------------------ 
 app = picoweb.WebApp(__name__)
@@ -129,7 +142,7 @@ def image(req, resp):
     print("Download JPG")
     yield from picoweb.start_response(resp)
     try:
-        with open("web/eau.jpg", 'rb') as img_binary:
+        with open("/web/eau.jpg", 'rb') as img_binary:
             img= img_binary.read()
         yield from resp.awrite(img)
     except Exception:
@@ -137,5 +150,6 @@ def image(req, resp):
         pass
     
 
-timer.init(period=60000, mode=Timer.PERIODIC, callback=handleInterrupt)
+timer.init(period=10000, mode=Timer.PERIODIC, callback=handleInterrupt)
+#timer.init(period=10000, mode=Timer.ONE_SHOT, callback=handleInterrupt)
 app.run(debug=True, host = ipaddress, port = 80)
